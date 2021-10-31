f78a691179b6e6cc4822688441a627962a8c6788
Everton Christmann <evertonluiz00@hotmail.com>
2021-10-31 16:25:52 -0300

    Enabling authentication in MongoDB

--- Catalog/Catalog.Api/Settings/MongoDbSettings.cs
+++ Catalog/Catalog.Api/Settings/MongoDbSettings.cs
@@ -9,10 +9,12 @@ namespace Catalog.Api
     {
         public string Host { get; set; }
         public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
 
         public string ConnectionString 
         {
-            get => $"mongodb://{ Host }:{ Port }";
+            get => $"mongodb://{ User }:{ Password }@{ Host }:{ Port }";
         }
     }
 }
